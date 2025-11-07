7ff567e1c76ef29aed5a0a4e138e779e73bf84a9
Azmi FH <azmialhaq17@gmail.com>
2025-11-07 22:53:40 +0700

    list

---
+++ src/app/List.kt
@@ -0,0 +1,15 @@
+package app
+
+fun main(){
+    val list : List<String> = listOf("azmi","fajarul","Haq")
+    //cara ambil data list
+    println(list.get(0))
+    //atau seperti array
+    println(list[1])
+    //cek posisi data di list
+    println(list.indexOf("fajarul"))
+    //list boolean atau untuk mengecek data pada list
+    println(list.contains("azmi"))
+    //empety cek apakah data kosong atau tidak
+    println(list.isEmpty())
+}
